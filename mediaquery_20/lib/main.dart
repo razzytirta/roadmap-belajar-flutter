4adc9733186d2dbe2302c1bc1b701b8effc44ea4
razzytirta <azzi.fachrurazzi@gmail.com>
2024-08-06 21:10:21 +0800

    mediaquery

---
+++ mediaquery_20/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(context),
+    );
+  }
+
+  Scaffold MainPage(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Belajar MediaQuery'),
+        backgroundColor: Colors.blue,
+      ),
+      body: (MediaQuery.of(context).orientation == Orientation.portrait)
+          ? Column(
+              children: generateContainers,
+            )
+          : Row(
+              children: generateContainers,
+            ),
+    );
+  }
+
+  List<Widget> get generateContainers {
+    return [
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.red,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.green,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.yellow,
+      ),
+    ];
+  }
+}
