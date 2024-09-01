8e753451011f605bbb65197c07c398f88d3459f3
razzytirta <azzi.fachrurazzi@gmail.com>
2024-09-02 05:04:58 +0800

    ClipPath

---
+++ clippath_34/android/app/src/main/kotlin/com/example/clippath_34/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clippath_34
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ clippath_34/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Belajar Clipper Path"),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+          child: ClipPath(
+            clipper: MyClipper(),
+            child: const Image(
+                width: 300,
+                image: NetworkImage(
+                    "https://fastly.picsum.photos/id/18/2500/1667.jpg?hmac=JR0Z_jRs9rssQHZJ4b7xKF82kOj8-4Ackq75D_9Wmz8")),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2, size.height * 0.75, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
