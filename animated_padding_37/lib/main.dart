7936eb910c7a23abd8a145c3064dab81c5bbc5e0
razzytirta <azzi.fachrurazzi@gmail.com>
2024-09-07 06:39:04 +0800

    Animated Padding

---
+++ animated_padding_37/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double myPadding = 5;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Animated Padding"),
+          centerTitle: true,
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: AnimatedPadding(
+                      padding: EdgeInsets.all(myPadding),
+                      duration: const Duration(seconds: 1),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            myPadding = 20;
+                          });
+                        },
+                        child: Container(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: AnimatedPadding(
+                      padding: EdgeInsets.all(myPadding),
+                      duration: const Duration(seconds: 1),
+                      child: Container(
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: AnimatedPadding(
+                      padding: EdgeInsets.all(myPadding),
+                      duration: const Duration(seconds: 1),
+                      child: Container(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: AnimatedPadding(
+                      padding: EdgeInsets.all(myPadding),
+                      duration: const Duration(seconds: 1),
+                      child: Container(
+                        color: Colors.yellow,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
